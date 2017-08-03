5e5b01fbd241c7146dca45d66c81a239f48073c1
joaotostes <joao_tostes17@hotmail.com>
2017-08-03 17:31:41 -0300

    Alteração exercicios MVC

---
+++ ExemplosValidation/ExemplosValidation/Controllers/PessoaController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExemplosValidation.Models;
+
+namespace ExemplosValidation.Controllers
+{
+    public class PessoaController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa();
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Pessoa pessoa)
+        {
+            //if (string.IsNullOrEmpty(pessoa.Nome))
+            //{
+            //    ModelState.AddModelError("Nome","O campo nome é obrigatório!");
+            //}
+            //if (pessoa.Senha != pessoa.ConfirmarSenha)
+            //{
+            //    ModelState.AddModelError("","As senhas não são iguais");
+            //}
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", pessoa);
+            }
+            return View(pessoa);
+        }
+        
+        public ActionResult Resultado(Pessoa pessoa)
+        {
+
+            return View(pessoa);
+        }
+    }
+}
---
+++ ExemplosValidation/ExemplosValidation/Models/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExemplosValidation.Models
+{
+    public class Pessoa
+    {
+        [Required(ErrorMessage = "Nome é um campo obrigatório!")]
+        public string Nome { get; set; }
+        [StringLength(50,MinimumLength = 5 , ErrorMessage = "Minimo 5 caracters, maxiomo 50" )]
+        public string Observacao { get; set; }
+        [Range(18,50,ErrorMessage = "A idade deve ser entre 18 e 50 anos")]
+        public int Idade { get; set; }
+        [RegularExpression(@"[A-Za-z\d_.-]+@[A-Za-z\d]+\.[a-z]{2,4}(\.[a-z]{2})?", ErrorMessage = "Email Invalido !!")]
+        public string Email { get; set; }
+        [RegularExpression(@"[a-zA-Z]{5,15}", ErrorMessage = "Login deve possuir somente Lentras de 5 a 15 caracters")]
+        [Required(ErrorMessage = "O login é obrigatório!")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Informe sua senha")]
+        public string Senha { get; set; }
+        [Compare("Senha",ErrorMessage = "As senhas não são compativeis")]
+        public string ConfirmarSenha { get; set; }
+    }
+}
