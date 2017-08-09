9e24ff918ed4a7ee33cf54f551dcd02ab2eca081
joaotostes <joao_tostes17@hotmail.com>
2017-08-09 17:56:08 -0300

    Banco de dados com MVC

---
+++ BD_CSharp/BD_CSharp/Contexto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace BD_CSharp
+{
+    class Contexto : IDisposable
+    {
+        private readonly SqlConnection minhaConexao;
+
+        public Contexto()
+        {
+            minhaConexao = new SqlConnection(ConfigurationManager);
+            minhaConexao.Open();
+        }
+
+        public void executaComando(string strQuery)
+        {
+            var cmdComando = new SqlCommand
+            {
+                CommandText = strQuery,
+                CommandType = CommandType.Text,
+                Connection = minhaConexao
+            };
+        }
+
+        public SqlDataReader ExecCmdComRetorno(string strQuery)
+        {
+            var cmdComando = new SqlCommand(strQuery, minhaConexao);
+            return cmdComando.ExecuteReader();
+        }
+
+        public void Dispose()
+        {
+            if (minhaConexao.State == ConnectionState.Open)
+                minhaConexao.Close();
+        }
+    }
+}
---
+++ BD_CSharp/BD_CSharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace BD_CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Criando string connection 
+            SqlConnection minhaConexao = new SqlConnection(@"data source=(localdb)\mssqllocaldb;Integrated Security=SSPI;Initial Catalog=BancoeMVC");
+            // abrindo conexao com banco de dados
+            minhaConexao.Open();
+
+            //string strQueryUpdate = "UPDATE Aluno SET Nome = 'Samara Leme' WHERE AlunoId = 1";
+            //SqlCommand cmdComandoUpdate = new SqlCommand(strQueryUpdate, minhaConexao);
+            //cmdComandoUpdate.ExecuteNonQuery();
+
+            //string strQueryDelete = "DELETE FROM Aluno WHERE AlunoId = 1005";
+            //SqlCommand cmdComandoDelete = new SqlCommand(strQueryDelete, minhaConexao);
+            //cmdComandoDelete.ExecuteNonQuery();
+
+            Console.WriteLine("Digite o Nome do Aluno");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite o Nome da Mãe do aluno");
+            string nomeMae = Console.ReadLine();
+            Console.WriteLine("Digite a data de Nascimento do aluno");
+            string dataNasc = Console.ReadLine();
+
+            string strQueryInsert = $"INSERT INTO Aluno (Nome,Mae,DataNascimento) VALUES ('{nome}','{nomeMae}','{dataNasc}')";
+            SqlCommand cmdComandoInsert = new SqlCommand(strQueryInsert, minhaConexao);
+            cmdComandoInsert.ExecuteNonQuery();
+
+
+            string strQuerySelect = "SELECT * FROM Aluno";
+            SqlCommand cmdComandoSelect = new SqlCommand(strQuerySelect, minhaConexao);
+            SqlDataReader dados = cmdComandoSelect.ExecuteReader();
+
+            while (dados.Read())
+            {
+                Console.WriteLine($"ID:{dados["AlunoId"]}, Nome:{dados["Nome"]}, Mae:{dados["Mae"]}, DataNascimento:{dados["DataNascimento"]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
